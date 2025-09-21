57d35f84d90afb98568b90cc2bad18db25c7a6f7
Alisa Tay <alisatay@mac.lan>
2025-09-21 10:30:23 -0700

    blank page

---
+++ my-frontend/src/NewPage.jsx
@@ -0,0 +1,8 @@
+export default function NewPage() {
+      return (
+      <main style ={{textAlign:"center",padding:"2rem"}}>
+      <h1>This is a blank page</h1>
+      <p>We can start adding content here</p>
+      </main>
+      );
+}
--- my-frontend/src/main.jsx
+++ my-frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import NewPage from './NewPage.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        {/* Home page */}
+        <Route path="/" element={<App />} />
+
+        {/* New blank page */}
+        <Route path="/new" element={<NewPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
