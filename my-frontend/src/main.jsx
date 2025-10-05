aef9528dc689634be7738c6a3a7c8e4b8019d7e5
Lily Hou <79422033+lilyh123@users.noreply.github.com>
2025-10-04 19:56:03 -0700

    Update main.jsx

--- my-frontend/src/main.jsx
+++ my-frontend/src/main.jsx
@@ -5,12 +5,8 @@ import './index.css'
 import App from './App';
 import NewPage from './NewPage';
 import ExoplanetPage from './exoplanetpage';
-
-
-createRoot(document.getElementById("root")).render(
-import App from './App.jsx'
-import NewPage from './NewPage.jsx'
 import MemberPage from './MemberPage.jsx'
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
@@ -20,22 +16,10 @@ createRoot(document.getElementById("root")).render(
 
         {/* New blank page */}
         <Route path="/new" element={<NewPage />} />
+
         <Route path="/exoplanetinfo" element={<ExoplanetPage />} />
+        <Route path="/mem" element={<MemberPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
-  < BrowserRouter >
-  <Routes>
-    {/* Home page */}
-    <Route path="/" element={<App />} />
-
-    {/* New blank page */}
-    <Route path="/new" element={<NewPage />} />
-
-    {/* Member page */}
-    <Route path="/mem" element={<MemberPage />} />
-  </Routes>
-    </BrowserRouter >
-  </StrictMode >
 );
