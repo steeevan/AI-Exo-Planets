455b2710874064ae065c6fae283e4cb028816e8a
Lily Hou <79422033+lilyh123@users.noreply.github.com>
2025-10-04 19:04:56 -0700

    3d model render

--- my-frontend/src/ExoplanetPage.jsx
+++ my-frontend/src/ExoplanetPage.jsx
@@ -1,19 +1,38 @@
-import { Carousel } from "flowbite-react";
+import { useEffect } from "react";
+import EmblaCarousel from "./EmblaCarousel";
+import ModelScene from "./ModelScene";
+
 function ExoplanetPage() {
-    return (
-        <div>
-        <p>What are exoplanets?</p>
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel>
-            <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-            <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-            <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-            <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-            <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-        </Carousel>
-        </div>
-        </div>
-    )
+
+  const OPTIONS = { dragFree: true, loop: true }
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+  const IMAGES = [
+    "https://flowbite.com/docs/images/carousel/carousel-1.svg",
+    "https://flowbite.com/docs/images/carousel/carousel-2.svg",
+    "https://flowbite.com/docs/images/carousel/carousel-3.svg",
+    "https://flowbite.com/docs/images/carousel/carousel-4.svg",
+    "https://flowbite.com/docs/images/carousel/carousel-5.svg",
+  ];
+
+  // useEffect(() => {
+  //   console.log(">>> IMAGES:", IMAGES);
+  // }, []);
+
+  return (
+    <div className="flex flex-col gap-14">
+      <h1>What are exoplanets?</h1>
+      <EmblaCarousel slides={IMAGES} options={OPTIONS} />
+      <p>paragraph paragraph paragraph paragraph paragraph paragraph paragraph
+        paragraph paragraph paragraph paragraph paragraph paragraph paragraph
+        paragraph paragraph paragraph paragraph paragraph paragraph paragraph
+        paragraph paragraph paragraph paragraph paragraph paragraph paragraph
+        paragraph paragraph paragraph paragraph paragraph paragraph paragraph
+      </p>
+      <ModelScene position={[0, 0, 0]} size={[1, 1, 1]} color={"red"} />
+    </div>
+  )
 };
 
 export default ExoplanetPage;
