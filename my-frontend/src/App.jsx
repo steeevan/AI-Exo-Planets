1a655b4515fb259ffa752701a721a33383628156
Alisa Tay <alisactay+github@gmail.com>
2025-10-05 18:19:29 -0700

    attempted to have carousel with rendered exoplanets

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -1,24 +1,13 @@
 
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NewPage from './NewPage'
+
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
